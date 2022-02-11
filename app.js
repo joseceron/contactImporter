64e0c9d684e43728b3a55dfed8919d0d270bb736
Jose Cerón <joseluisceron13@gmail.com>
2022-02-11 12:13:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const userRouter = require('./routers/contacts')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization");
+    next();
+});
+app.use(userRouter)
+
+module.exports = app
