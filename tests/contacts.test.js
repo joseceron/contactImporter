e820e5d402cb01849acdc5c01a606471cf3318fb
Jose Cerón <joseluisceron13@gmail.com>
2022-02-14 00:07:23 -0500

    [fix] update test payload because of a change in the api

--- tests/contacts.test.js
+++ tests/contacts.test.js
@@ -38,7 +38,7 @@ test('Should get contacts from DB', (done) => {
 
 })
 
-let files = [ { "fileName": "contacts7.csv", "contacts": "client1,19891104,(+57) 320 561 40 51,address1,371449635398431,client1@gmail.com\r\nclient2,1957-12-01,(+57) 323-561-40-51,address2,30569309025904,client2@gmail.com\r\nclient3.,20000512,(+57) 323-561-40-51,address3,6011111111111117,client3@gmail.com\r\n,19891401,(+57) 323-561-40-511,address4,3530111333300000,client4\r\nclient5,,+57 320 672 81 72,,5555555555554444,jose@outlook.com\r\nclient6 lastname,20010928,(+0) 320 78 81 18,parker st 67 NE3300,123,lina@yahoo\r\nclient7,,,,,client1@gmail.com\r\nclient8,19891104,(+57) 310 129 10 99,address8,371449635398431,client8@gmail.com\r\nclient8,19201001,(+57) 311 109 20 17,address9,371449635398431,client9@gmail.com", "userToken": "ingFghPo28b4usFOj37fVUg77SP2" } ]
+let files = [{"fileName":"contacts7.csv","headers":["name","dof","phone","address","credit_card","email"],"contacts":"client1,19891104,(+57) 320 561 40 51,address1,371449635398431,client1@gmail.com\r\nclient2,1957-12-01,(+57) 323-561-40-51,address2,30569309025904,client2@gmail.com\r\nclient3.,20000512,(+57) 323-561-40-51,address3,6011111111111117,client3@gmail.com\r\n,19891401,(+57) 323-561-40-511,address4,3530111333300000,client4\r\nclient5,,+57 320 672 81 72,,5555555555554444,jose@outlook.com\r\nclient6 lastname,20010928,(+0) 320 78 81 18,parker st 67 NE3300,123,lina@yahoo\r\nclient7,,,,,client1@gmail.com\r\nclient8,19891104,(+57) 310 129 10 99,address8,371449635398431,client8@gmail.com\r\nclient8,19201001,(+57) 311 109 20 17,address9,371449635398431,client9@gmail.com","userToken":"ingFghPo28b4usFOj37fVUg77SP2"}]
 test('should process files', async() => {
   await request(app)
     .post('/contacts')
